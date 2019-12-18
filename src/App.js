6455d3886038e98f5f8aa703ae240690fbce7cb4
Ashish Kumar Lahoti <lahoti.ashish20@gmail.com>
2019-12-18 18:57:32 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import ColorConverter from "./ColorConverter.js";
+import Home from "./Home.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">
+              Coding N Concepts
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/color-converter">Color Converter</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/color-converter" component={ColorConverter} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
